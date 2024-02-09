1f66b066bbd0c3bb54c8fec0d8db8f815125c0ab
Jessé Alves <jessealvespr@outlook.com>
2024-02-09 18:33:21 -0300

    exercicio1

---
+++ 01/index.js
@@ -0,0 +1,20 @@
+const endereco = {
+    rua: 'Rua dos bobos',
+    numero: '0',
+    complemento: 'Casa engraçada',
+    bairro: 'Esmero',
+    cep: 83525000,
+    cidade: 'Sem teto',
+    estado: 'Sem parede',
+    pais: 'Pinico'
+}
+
+const usuario = {
+    nome: 'Leoncio',
+    email: 'leoncio@gmail.com.br',
+    telefone: 5550123252,
+    data_nascimento: 07061945,
+    endereco,
+}
+
+console.log(usuario);
