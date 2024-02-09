e3a1c51e63ead05e3a4e817a7d8399adf4eb1359
Jessé Alves <jessealvespr@outlook.com>
2024-02-09 18:38:35 -0300

    exercicio2

---
+++ 02/index.js
@@ -0,0 +1,28 @@
+const carros = [
+    {
+        marca: "Fiat",
+        modelo: "Palio",
+        ano: 2015,
+        cor: "Roxo",
+        quantidade_portas: 2,
+        automatico: false
+    },
+    {
+        marca: "Volvo",
+        modelo: "XC40",
+        ano: 2024,
+        cor: "Branco",
+        quantidade_portas: 4,
+        automatico: true
+    },
+    {
+        marca: "Ford",
+        modelo: "Bronco",
+        ano: 2023,
+        cor: "Preto",
+        quantidade_portas: 4,
+        automatico: true
+    }
+]
+
+console.log(carros);
